e616911484d45ddba308c45c5c9e3aa3c74142ec
iamrosada <luisrosada2018@gmail.com>
2023-11-22 11:41:27 +0300

    feat: created the postgres repository with all methods cruds

--- promocode-management/user-service/internal/user/infra/repository/user_repositoy_postgres.go
+++ promocode-management/user-service/internal/user/infra/repository/user_repositoy_postgres.go
@@ -17,3 +17,27 @@ func NewUserRepositoryPostgres(db *gorm.DB) *UserRepositoryPostgres {
 func (r *UserRepositoryPostgres) Create(user *entity.User) error {
 	return r.DB.Create(user).Error
 }
+
+func (r *UserRepositoryPostgres) FindAll() ([]*entity.User, error) {
+	var users []*entity.User
+	if err := r.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *UserRepositoryPostgres) Update(user *entity.User) error {
+	return r.DB.Save(user).Error
+}
+
+func (r *UserRepositoryPostgres) DeleteByID(id string) error {
+	return r.DB.Where("id = ?", id).Delete(entity.User{}).Error
+}
+
+func (r *UserRepositoryPostgres) GetByID(id string) (*entity.User, error) {
+	var user entity.User
+	if err := r.DB.Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
