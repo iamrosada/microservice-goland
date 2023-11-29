886f8eb694e118ddef9fe749ee5b0a42eeb7fd02
iamrosada <luisrosada2018@gmail.com>
2023-11-29 13:21:32 +0300

    feat: created promo repository functions

--- promo-service/internal/promo_code/infra/repository/promo_repository.go
+++ promo-service/internal/promo_code/infra/repository/promo_repository.go
@@ -1,10 +1,9 @@
-// infra/promo_repository.go
 package repository
 
 import (
 	"fmt"
-	"math/rand"
 	"strings"
+	"time"
 
 	"github.com/iamrosada/microservice-goland/promo-service/internal/promo_code/entity"
 	"gorm.io/gorm"
@@ -36,7 +35,7 @@ func NewPromoRepository(db *gorm.DB) *PromoRepositoryImpl {
 }
 
 func (r *PromoRepositoryImpl) Create(promo *entity.Promotion) error {
-	return r.DB.Create(&promo).Error
+	return r.DB.Create(promo).Error
 }
 
 func (r *PromoRepositoryImpl) FindByID(id uint) (*entity.Promotion, error) {
@@ -47,22 +46,10 @@ func (r *PromoRepositoryImpl) FindByID(id uint) (*entity.Promotion, error) {
 	}
 	return &promo, nil
 }
+
 func (r *PromoRepositoryImpl) FindCodesByPromotionID(promotionID uint) ([]string, error) {
-
 	var codes []string
-
-	// Executando uma query SQL personalizada para selecionar os valores da coluna "Codes"
-	err := r.DB.Raw("SELECT Codes FROM codes_promos WHERE promotion_id = ?", promotionID).Pluck("Codes", &codes).Error
-	if err != nil {
-		return nil, err
-	}
-
-	return codes, nil
-}
-
-func (r *PromoRepositoryImpl) FindAllCodes() ([]entity.CodesPromo, error) {
-	var codes []entity.CodesPromo
-	err := r.DB.Find(&codes).Error
+	err := r.DB.Table("codes_promos").Select("Codes").Where("promotion_id = ?", promotionID).Pluck("Codes", &codes).Error
 	if err != nil {
 		return nil, err
 	}
@@ -70,40 +57,28 @@ func (r *PromoRepositoryImpl) FindAllCodes() ([]entity.CodesPromo, error) {
 }
 
 func (r *PromoRepositoryImpl) AddCodes(id uint, codes []string) error {
-	// Find the promotion by ID
 	promo, err := r.FindByID(id)
 	if err != nil {
 		return err
 	}
 
-	// Assuming that there is a one-to-many relationship between Promotion and CodesPromo
-	// Create a map to check for duplicate codes
 	codeMap := make(map[string]bool)
-
-	// Check for duplicates (case-insensitive) and add each unique code to the promotion
 	for _, codeValue := range codes {
-		// Convert the code to lowercase for case-insensitive comparison
 		lowercaseCode := strings.ToLower(codeValue)
 
-		// Check if the code is already in the map (duplicate)
 		if _, exists := codeMap[lowercaseCode]; exists {
-			// Handle duplicate code error
 			return fmt.Errorf("duplicate code found: %s", codeValue)
 		}
 
-		// Add the lowercase code to the map
 		codeMap[lowercaseCode] = true
 		newID := generateNewID()
 
-		// Assign the generated ID to the promotion
-		// Create the CodesPromo entity
 		code := entity.CodesPromo{
 			ID:          newID,
 			Codes:       []string{codeValue},
-			PromotionID: promo.ID, // Assuming Promotion has an ID field
+			PromotionID: promo.ID,
 		}
 
-		// Save the code to the database
 		if err := r.DB.Create(&code).Error; err != nil {
 			return err
 		}
@@ -113,10 +88,9 @@ func (r *PromoRepositoryImpl) AddCodes(id uint, codes []string) error {
 }
 
 func (r *PromoRepositoryImpl) CreateUserThatWillUsePromo(promo *entity.UserPromotion) error {
-	return r.DB.Create(&promo).Error
+	return r.DB.Create(promo).Error
 }
 
 func generateNewID() uint {
-
-	return uint(rand.Uint32())
+	return uint(time.Now().UnixNano())
 }
